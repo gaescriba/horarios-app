6e5bab3a365b335240575dca30eb53d359729f47
gaescriba <58343999+gaescriba@users.noreply.github.com>
2023-06-26 02:59:41 -0400

    deleted node_modules

--- frontend-horarios/node_modules/expo-checkbox/build/ExpoCheckbox.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { CheckboxProps } from './Checkbox.types';
-export default class ExpoCheckbox extends React.PureComponent<CheckboxProps> {
-    private handleChange;
-    render(): JSX.Element;
-}
-//# sourceMappingURL=ExpoCheckbox.d.ts.map
