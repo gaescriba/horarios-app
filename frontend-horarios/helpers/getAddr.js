78d0b09e265c7219719f9382f2f354b7c0af565b
gaescriba <58343999+gaescriba@users.noreply.github.com>
2023-06-26 04:39:54 -0400

    Updated getAddr.js

--- frontend-horarios/helpers/getAddr.js
+++ frontend-horarios/helpers/getAddr.js
@@ -1,20 +1,6 @@
 export const getAddr = () => {
 
-  let addr = 0
-  let option = 1 
-  
-  switch(option){
-    case 1:
-      addr = '192.168.0.10'
-    break
-    case 2:
-      addr = '192.168.106.63' 
-    break
-    case 3:
-      addr = '10.28.17.235' 
-    break
-    
-  }
+  let addr = 'your.local.ip.address'
 
   return addr
 }
